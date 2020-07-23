6387560258c4d13e1e132eaa44374069e1d79e28
Aman <devproddeploy@gmail.com>
2020-07-23 18:13:31 +0530

    functionaliy finished

--- src/components/Group/Groups.jsx
+++ src/components/Group/Groups.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect, useRef, Fragment, useContext } from 'react'
 import FlickerContext from '../../context/flicker/flickerContext';
 import GroupItem from './GroupItem';
 import PieChart from './PieChart';
-import M from 'materialize-css/dist/js/materialize';
 
 const Groups = () => {
 
@@ -27,29 +26,6 @@ const Groups = () => {
         })
     }
 
-    const auto = useRef();
-
-    useEffect(() => {
-        if(auto.current){
-            M.Autocomplete.init(auto.current, {
-                data: {
-                    "cars": null,
-                    "technology": null,
-                    "entertainment": null,
-                    "apple": null,
-                    "movies": null,
-                    "artists": null,
-                    "art": null,
-                    "hollywood": null,
-                    "london": null,
-                    "rain": null,
-                    "balloons": null,
-                    "black": null,
-                }
-            });
-        }
-    }, [auto])
-
     const { group } = formData;
 
     return (
@@ -59,14 +35,10 @@ const Groups = () => {
                 <p className="flow-text">Search Groups</p>
             </div>
             <form onSubmit={onSubmit}>
-                <div className="input-field">
-                <input ref={auto} type="text" id="autocomplete-input" class="autocomplete" name='group' onChange={onChange} value={group} />
-          <label htmlFor="autocomplete-input">Autocomplete</label>
-                </div>
-            {/* <div className="input-field">
+            <div className="input-field">
                 <input type="text" name='group' onChange={onChange} value={group}/>
                 <label htmlFor="group">Search Group</label>
-            </div> */}
+            </div>
             <div className="input-field">
                 <button type="submit" className="btn green">Search</button>
             </div>
